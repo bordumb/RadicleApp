1c51bfaae5b8f8a31bff7c1de34bb051f19e4d36
bordumb <bordumbb@gmail.com>
2025-02-16 16:25:37 +0000

    Initial Commit

---
+++ RadicleAppTests/RadicleAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  RadicleAppTests.swift
+//  RadicleAppTests
+//
+//  Created by bordumb on 16/02/2025.
+//
+
+import Testing
+@testable import RadicleApp
+
+struct RadicleAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
